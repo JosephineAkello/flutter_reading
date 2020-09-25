2d21ffcaba382755e7b3cc9a5e2c4d79f3288ccb
JosephineAkello <Josephineakello2030@gmail.com>
2020-09-25 15:17:09 +0300

    add error

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return Scaffold(
       title: 'An app that can count...',
       theme: ThemeData(
         // This is the theme of your application.
