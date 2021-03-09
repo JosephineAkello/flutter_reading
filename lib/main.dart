d3ab4b6c642f71037fe028d4f7f118014ba9182d
JosephineAkello <Josephineakello2030@gmail.com>
2021-03-09 06:03:43 +0300

    sentry changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,26 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
+// import 'dart:async';
 import 'package:sentry/sentry.dart';
 
-final sentry = SentryClient(dsn: "https://13a0e9c0fe854b81843c8a620dc265c2@o433262.ingest.sentry.io/5436444");
+final sentry = SentryClient(dsn: "https://3862962d7d6b4ffd92d0bb3bbeeae95b@o433262.ingest.sentry.io/5511493");
 
-bool get isInDebugMode {
-  bool inDebugMode = false;
-  assert(inDebugMode = true);
-  return inDebugMode;
-}
-
-Future<Null> _reportError(dynamic error, dynamic stackTrace) async {
-  print('Caught error: $error');
-
-
-    if (isInDebugMode) {
-    print(stackTrace);
-    print('In dev mode. Not sending report to Sentry.io.');
-    return;
-  }
-
-  print('Reporting to Sentry.io...');
-
-
-  final SentryResponse response = await sentry.captureException(
+void main() async {
+ try {
+  throw null;
+} catch (error, stackTrace) {
+  await sentry.captureException(
     exception: error,
     stackTrace: stackTrace,
   );
-
-  if (response.isSuccessful) {
-    print('Success! Event ID: ${response.eventId}');
-  } else {
-    print('Failed to report to Sentry.io: ${response.error}');
-  }
 }
 
-Future<Null> main() async {
-  // This captures errors reported by the Flutter framework.
-  FlutterError.onError = (FlutterErrorDetails details) async {
-    if (isInDebugMode) {
-      // In development mode simply print to console.
-      FlutterError.dumpErrorToConsole(details);
-    } else {
-      // In production mode report to the application zone to report to
-      // Sentry.
-      Zone.current.handleUncaughtError(details.exception, details.stack);
-    }
-  };
 
-    runZonedGuarded<Future<Null>>(() async {
-    runApp(new MyApp());
-  }, (error, stackTrace) async {
-    await _reportError(error, stackTrace);
-  });
+FlutterError.onError = (details, {bool forceReport = false}) {
+  sentry.captureException(
+    exception: details.exception,
+    stackTrace: details.stack,
+  );
+};
 }
 
 class MyApp extends StatelessWidget {
